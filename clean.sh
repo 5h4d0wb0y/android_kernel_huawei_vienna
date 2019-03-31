48867b7e418fec4b838229b9dd89c7d426649461
5h4d0wb0y <5h4d0wb0y@protonmail.com>
2019-03-31 18:56:54 +0200

    implemented nethunter-devices build scripts and configure.sh to download toolchain

--- clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make ARCH=arm64 distclean
-rm -rf out/
-rm -rf toolchain64/
---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Check if toolchain exists
+if [ ! -d $HOME/toolchain ]; then
+    git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b oreo-release $HOME/toolchain
+fi
